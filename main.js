2f78c9f392fa189f2c718ea7f394787564f302cf
JasonXZLiu <jason.XZ.liu@gmail.com>
2016-10-09 21:10:10 -0400

    added mainpage

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function () {
 	var $el = $(".wordfade:first"), text = $.trim($el.text()),
 	    words = text.split(" "), html = "";
@@ -19,25 +18,3 @@ $(document).ready(function () {
 	$('.prompter').fadeIn(18000).removeClass('hidden');
     $('.btn.hidden').fadeIn(10000).removeClass('hidden');
 });
-=======
-function grabCursor() {
-	var tb_mode = jQuery(".tb_mode_cb").prop("checked");
-		jQuery("body").addClass("touch_mode_grab");
-		jQuery("body").mousedown(function() {
-			jQuery(this).removeClass("touch_mode_grab")
-				.addClass("touch_mode_grabbing");
-		}).mouseup(function() {
-			jQuery(this).removeClass("touch_mode_grabbing")
-				.addClass("touch_mode_grab");
-		});
-	return this;
-}
-
-jQuery(document).ready(function($) {
-	$(".tb_mode_cb").prop("checked", false);
-	grabCursor();
-	$(".tb_mode_cb").change(function() {
-		grabCursor();
-	});
-});
->>>>>>> origin/master
